7bd2b74652fa638a085170b9f61d3e2b5c63d738
zhang_haopai <zhang_haopai@venusgroup.com.cn>
2025-09-12 10:14:18 +0800

    提交postgresql

--- installer/cape2.sh
+++ installer/cape2.sh
@@ -922,6 +922,9 @@ function install_postgresql() {
     sudo apt-get update
     sudo apt-get install -y libpq-dev postgresql postgresql-client
 
+    sudo mkdir mkdir -p /var/run/postgresql
+    sudo chown -R postgres:postgres /var/run/postgresql
+
     sudo systemctl enable postgresql.service
     sudo systemctl start postgresql.service
 
